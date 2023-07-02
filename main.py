a8729dd05e7b5d53617d6416919f18e56b7b218d
hongwen000 <hongwen000@outlook.com>
2023-07-02 15:48:32 +0800

    better

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from PyHotKey import Key, keyboard_manager as manager
-
 import pygetwindow as gw
 import tkinter as tk
 from pynput import keyboard
 import re
 import ctypes
 import pyautogui
-
 
 class App:
 
@@ -15,40 +13,55 @@ class App:
         self.entry_var = tk.StringVar()
         self.entry_var.trace("w", self.update_list)
         self.entry = tk.Entry(root, textvariable=self.entry_var)
+        self.entry.configure(font=("Helvetica", 14))
         self.vscode_windows = self.get_vscode_windows()
-        self.listbox = tk.Listbox(root)
+        self.listbox = tk.Listbox(root, font=("Helvetica", 14), width=40)
         self.update_list()
         self.entry.bind('<Return>', self.switch_window)
         self.entry.pack()
         self.listbox.pack()
         self.this_program_window = None
 
-
         self.switch_button = tk.Button(root, text="Switch", command=self.switch_window_button)
         self.switch_button.pack()
-        # self.root.attributes('-topmost', True)  # Add this line
         id1 = manager.register_hotkey([Key.alt_l, Key.space], None, self.activate)
         manager.suppress = True
-        self.root.title("My Switcher Program")  # 设置程序窗口标题
+        
+        self.root.title("My Switcher Program")
         self.entry.focus()
-        # 获取并存储自身的窗口
 
+        # 隐藏标题栏
+        self.root.overrideredirect(True)
 
+        # 将窗口居中
+        window_width = self.root.winfo_reqwidth() + 500
+        window_height = self.root.winfo_reqheight()
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
 
+        x_coord = screen_width // 2 - window_width // 2
+        y_coord = screen_height // 2 - window_height // 2
+
+        self.root.geometry(f'{window_width}x{window_height}+{x_coord}+{y_coord}')
+        
 
     def activate(self):
-        # 激活程序窗口
-        this_program_window = [win for win in gw.getAllWindows() if "My Switcher Program" in win.title][0]
+        self.update_vscode_windows()  # 更新 vscode 窗口列表
+        this_program_window = self.get_this_program_window()
         print(this_program_window)
         this_program_window.minimize()
         this_program_window.restore()
-        # 设置焦点在输入框上
         self.entry.focus()
-
-
 
     def get_vscode_windows(self):
         return [win for win in gw.getAllWindows() if "Visual Studio Code" in win.title]
+
+    def update_vscode_windows(self):
+        self.vscode_windows = self.get_vscode_windows()
+        self.update_list()
+
+    def get_this_program_window(self):
+        return [win for win in gw.getAllWindows() if "My Switcher Program" in win.title][0]
 
     def update_list(self, *args):
         search = self.entry_var.get()
@@ -60,7 +73,7 @@ class App:
             self.listbox.select_set(0)
 
     def switch_window(self, event):
-        this_program_window = [win for win in gw.getAllWindows() if "My Switcher Program" in win.title][0]
+        this_program_window = self.get_this_program_window()
         this_program_window.minimize()
         selection = self.listbox.curselection()
         if selection:
