2799d03df53ec5cb3e8911561114c3a69fa47bb8
V3GWR36YG <hongwen000@outlook.com>
2023-07-05 20:06:35 +0800

    All

--- main.py
+++ main.py
@@ -5,6 +5,100 @@ from pynput import keyboard
 import re
 import ctypes
 import pyautogui
+from PIL import Image, ImageDraw
+import win32con
+import wordninja
+import win32api
+import ctypes
+from win32api import (
+    GetCurrentThreadId
+)
+from ctypes import *
+import ctypes
+import ctypes.wintypes
+from ctypes import (
+    POINTER,
+    Structure
+)
+from ctypes.wintypes import (
+    DWORD,
+    LONG,
+    WORD,
+    BYTE,
+    RECT,
+    UINT,
+    ATOM
+)
+class tagTITLEBARINFO(Structure):
+    pass
+tagTITLEBARINFO._fields_ = [
+    ('cbSize', DWORD),
+    ('rcTitleBar', RECT),
+    ('rgstate', DWORD * 6),
+]
+PTITLEBARINFO = POINTER(tagTITLEBARINFO)
+LPTITLEBARINFO = POINTER(tagTITLEBARINFO)
+TITLEBARINFO = tagTITLEBARINFO
+
+class tagWINDOWINFO(Structure):
+
+    def __str__(self):
+        return '\n'.join([key + ':' + str(getattr(self, key)) for key, value in self._fields_])
+
+tagWINDOWINFO._fields_ = [
+    ('cbSize', DWORD),
+    ('rcWindow', RECT),
+    ('rcClient', RECT),
+    ('dwStyle', DWORD),
+    ('dwExStyle', DWORD),
+    ('dwWindowStatus', DWORD),
+    ('cxWindowBorders', UINT),
+    ('cyWindowBorders', UINT),
+    ('atomWindowType', ATOM),
+    ('wCreatorVersion', WORD),
+]
+WINDOWINFO = tagWINDOWINFO
+LPWINDOWINFO = POINTER(tagWINDOWINFO)
+PWINDOWINFO = POINTER(tagWINDOWINFO)
+
+def is_alt_tab_window(hwnd):
+    """Check whether a window is shown in alt-tab.
+
+    See http://stackoverflow.com/a/7292674/238472 for details.
+    """
+    hwnd_walk = win32con.NULL
+    hwnd_try = ctypes.windll.user32.GetAncestor(hwnd, win32con.GA_ROOTOWNER)
+    while hwnd_try != hwnd_walk:
+        hwnd_walk = hwnd_try
+        hwnd_try = ctypes.windll.user32.GetLastActivePopup(hwnd_walk)
+        if gw.Win32Window(hwnd_try).visible:
+            break
+
+    if hwnd_walk != hwnd:
+        return False
+
+    # the following removes some task tray programs and "Program Manager"
+    ti = TITLEBARINFO()
+    ti.cbSize = ctypes.sizeof(ti)
+    ctypes.windll.user32.GetTitleBarInfo(hwnd, ctypes.byref(ti))
+    if ti.rgstate[0] & win32con.STATE_SYSTEM_INVISIBLE:
+        return False
+
+    # Tool windows should not be displayed either, these do not appear in the
+    # task bar.
+    if win32api.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) & win32con.WS_EX_TOOLWINDOW:
+        return False
+
+    pwi = WINDOWINFO()
+    windll.user32.GetWindowInfo(hwnd, byref(pwi))
+    # A top-level window created with this style does not become the foreground
+    # window when the user clicks it. The system does not bring this window to
+    # the foreground when the user minimizes or closes the foreground window.
+    # The window does not appear on the taskbar by default.
+    if pwi.dwExStyle & win32con.WS_EX_NOACTIVATE:
+        return False
+
+    return True
 
 class App:
 
@@ -14,7 +108,7 @@ class App:
         self.entry_var.trace("w", self.update_list)
         self.entry = tk.Entry(root, textvariable=self.entry_var)
         self.entry.configure(font=("Helvetica", 14))
-        self.vscode_windows = self.get_vscode_windows()
+        self.window_list = self.get_windows()
         self.listbox = tk.Listbox(root, font=("Helvetica", 14), width=40)
         self.update_list()
         self.entry.bind('<Return>', self.switch_window)
@@ -28,7 +122,7 @@ class App:
         id1 = manager.register_hotkey([Key.alt_l, Key.space], None, self.activate)
         manager.suppress = True
         
-        self.update_vscode_windows()  # 更新 vscode 窗口列表
+        self.update_window_list()  # 更新 vscode 窗口列表
         self.root.title("My Switcher Program")
         self.entry.focus()
 
@@ -48,28 +142,83 @@ class App:
         
 
     def activate(self):
-        self.update_vscode_windows()  # 更新 vscode 窗口列表
+        self.update_window_list()  # 更新 vscode 窗口列表
         this_program_window = self.get_this_program_window()
-        print(this_program_window)
         this_program_window.minimize()
         this_program_window.restore()
         self.entry.focus()
 
-    def get_vscode_windows(self):
-        return [win for win in gw.getAllWindows() if "Visual Studio Code" in win.title]
+    def get_windows(self):
+        win: gw.Win32Window
+        return [win for win in gw.getAllWindows() if (len(win.title) > 0 and is_alt_tab_window(win._hWnd))]
 
-    def update_vscode_windows(self):
-        self.vscode_windows = self.get_vscode_windows()
-        for w in self.vscode_windows:
-            print(w.title)
+    def update_window_list(self):
+        self.window_list = self.get_windows()
+        # for w in self.window_list:
+        #     print(w.title)
         self.update_list()
 
     def get_this_program_window(self):
         return [win for win in gw.getAllWindows() if "My Switcher Program" in win.title][0]
 
     def get_acronym_score(self, search, title):
-        words = re.findall(r'\b\w', title) 
-        acronym = ''.join(words)
+        words = [" ".join(wordninja.split(part)) for part in title.split(" - ")]
+        title = " ".join(words)
+        words = list(filter(lambda x: len(x) > 0, title.split(" ")))
+        map_acronym_to_word = [{word[0]: word} for word in words]
+        acronym = [word[0] for word in words]
+
+        # Start searching
+        matched_length = self.search_acronym(search, acronym, map_acronym_to_word)
+        return matched_length  # Increase the score by matched length squared
+
+
+    def search_acronym(self, search, acronym, map_acronym_to_word, search_idx=0, acronym_idx=0):
+        # Recursion base case: if all search chars are found, return len(search)
+        if search_idx == len(search):
+            return len(search)
+        
+        # Recursion base case: if all acronyms are used and search not found, return 0
+        if acronym_idx == len(acronym):
+            return 0
+
+        max_match_len = 0
+        # If this acronym char match this search char
+        if search[search_idx].lower() == acronym[acronym_idx].lower():
+            # Try to match next search char in this word first (greedy)
+            word = map_acronym_to_word[acronym_idx][acronym[acronym_idx]]
+            word_match_len = self.search_in_word(search, search_idx, word)
+
+            # Try to match rest of search string with rest of acronyms
+            for end_idx in range(search_idx + 1, search_idx + word_match_len + 1):
+                next_match_len = self.search_acronym(search, acronym, map_acronym_to_word, end_idx, acronym_idx + 1)
+                max_match_len = max(max_match_len, (end_idx - search_idx) + next_match_len)
+
+        # Also try without matching this search char and move to next acronym
+        max_match_len = max(max_match_len, self.search_acronym(search, acronym, map_acronym_to_word, search_idx, acronym_idx + 1))
+
+        return max_match_len
+
+    def search_in_word(self, search, search_idx, word):
+        # Search for search[search_idx:] in word, return the length of matched substring
+        word_idx = 0
+        matched_len = 0
+        while search_idx < len(search) and word_idx < len(word):
+            if search[search_idx].lower() == word[word_idx].lower():
+                matched_len += 1
+                search_idx += 1
+            word_idx += 1
+        return matched_len
+
+
+    def get_acronym_score_old(self, search, title):
+        # print(title)
+        words = [" ".join(wordninja.split(part)) for part in title.split(" - ")]
+        # print(words)
+        title = " ".join(words)
+        word_starts = re.findall(r'\b\w', title) 
+        # print(word_starts)
+        acronym = ''.join(word_starts)
 
         search_idx = 0
         for idx, char in enumerate(acronym):
@@ -81,23 +230,51 @@ class App:
 
         return search_idx if search_idx == len(search) else 0
 
+    def remove_vscode_postfix(self, title: str) -> str:
+        parts = title.split(" - ")
+        if len(parts) > 3:
+            # Remove the last two parts (profile name and "Visual Studio Code")
+            parts = parts[:-2]
+        else:
+            # Remove the last part ("Visual Studio Code")
+            parts = parts[:-1]
+        return " - ".join(parts)
+
+
     def update_list(self, *args):
         search = self.entry_var.get()
+        not_vscode, is_vscode = [], []
+        is_vscode = [win for win in self.window_list if "Visual Studio Code" in win.title]
+        not_vscode = [win for win in self.window_list if win not in is_vscode]
         if len(search) == 0:
             self.listbox.delete(0, tk.END)
-            for win in self.vscode_windows:
-                self.listbox.insert(tk.END, win.title)
+            for win in (is_vscode + not_vscode):
+                # title = self.remove_vscode_postfix(win.title)
+                title = win.title
+                self.listbox.insert(tk.END, title)
         else:
             keywords = search.split()
             score_list = []
-
-            for i, win in enumerate(self.vscode_windows):
-                base_score = sum(int(re.search(keyword, win.title, re.I) is not None) for keyword in keywords)
             
-                acronym_score = self.get_acronym_score(search, win.title)
+
+            for i, win in enumerate(is_vscode):
+                search_title = self.remove_vscode_postfix(win.title)
+                orig_title = win.title
+                base_score = sum(int(re.search(keyword, search_title, re.I) is not None) for keyword in keywords)
+                base_score *= 100
+            
+                acronym_score = self.get_acronym_score(search, search_title)
 
                 score = base_score + acronym_score
-                score_list.append((score, win.title))
+                score_list.append((score, orig_title))
+
+            for i, win in enumerate(not_vscode):
+                search_title = win.title
+                orig_title = win.title
+                base_score = sum(int(re.search(keyword, search_title, re.I) is not None) for keyword in keywords)
+                base_score *= 100
+                score = base_score + 0
+                score_list.append((score, orig_title))
 
             score_list.sort(reverse=True, key=lambda x: x[0])
 
@@ -105,6 +282,7 @@ class App:
             for score, title in score_list:
                 if score > 0:
                     self.listbox.insert(tk.END, title)
+                    print(f"{title}: {score}")
 
         if len(self.listbox.get(0, tk.END)) > 0:
             self.listbox.select_set(0)
@@ -115,7 +293,7 @@ class App:
 
         if selection:
             title = self.listbox.get(selection[0])
-            for win in self.vscode_windows:
+            for win in self.window_list:
                 if win.title == title:
                     win.restore()
                     win.activate()
@@ -134,11 +312,45 @@ class App:
     def switch_window_button(self):
         self.switch_window(None)
 
+import pystray
+import threading
+import time, sys
+
 def main():
     root = tk.Tk()
     app = App(root)
+    quit_event = threading.Event()
+    # 创建一个新的白色图片
+    image = Image.new('RGB', (64, 64), color = 'white')
+
+    # 在图片上绘制一个黑色的矩形
+    draw = ImageDraw.Draw(image)
+    draw.rectangle(
+        [(10, 10), (54, 54)],
+        fill='black'
+    )
+    def run_icon():
+        icon.run()
+
+    def exit_action(icon, item):
+        icon.stop()
+        quit_event.set()
+        root.quit()
+        sys.exit()
+        root.destroy()
+
+    menu = (pystray.MenuItem('Exit', exit_action),)
+    # 现在你可以使用这个图片作为你的系统托盘图标
+    icon = pystray.Icon("name", image, "My System Tray Icon", menu)
+    icon_thread = threading.Thread(target=run_icon)
+    icon_thread.start()
 
     root.mainloop()
+    # while not quit_event.is_set():
+    #     root.update()
+    #     root.update_idletasks()
+    #     time.sleep(1.0 / 60.0)
+
 
 from ctypes import windll
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,19 +1,83 @@
-import subprocess
+# import subprocess
+# import sys
+
+# def install(package):
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# def create_executable(script):
+#     subprocess.check_call([sys.executable, "-m", "PyInstaller", "--onefile", "--windowed", script])
+
+# if __name__ == "__main__":
+#     # 安装需要的包
+#     install("PyInstaller")
+#     install("PyHotKey")
+#     install("pygetwindow")
+#     install("pynput")
+#     install("pyautogui")
+
+#     # 创建可执行文件
+#     create_executable("main.py")
+
+import os
 import sys
+import ctypes
+from ctypes import wintypes, windll
+import wpath
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+from PIL import Image, ImageDraw
 
-def create_executable(script):
-    subprocess.check_call([sys.executable, "-m", "PyInstaller", "--onefile", "--windowed", script])
+image = Image.new('RGB', (64, 64), color = 'white')
 
-if __name__ == "__main__":
-    # 安装需要的包
-    install("PyInstaller")
-    install("PyHotKey")
-    install("pygetwindow")
-    install("pynput")
-    install("pyautogui")
+# 在图片上绘制一个黑色的矩形
+draw = ImageDraw.Draw(image)
+draw.rectangle(
+    [(10, 10), (54, 54)],
+    fill='black'
+)
 
-    # 创建可执行文件
-    create_executable("main.py")
+# 保存为.ico文件
+image.save('icon.ico')
+
+import os
+import sys
+import winshell
+from pathlib import Path
+from wpath import FOLDERID
+import wpath
+
+# 获取Python解释器的路径
+python_path = sys.executable
+
+# 获取pythonw.exe的路径
+pythonw_path = os.path.join(os.path.dirname(python_path), "pythonw.exe")
+
+# 获取main.py的路径
+main_path = os.path.join(os.getcwd(), "main.py")
+
+# 获取桌面的路径
+desktop = Path(wpath.get_path(FOLDERID.Desktop))
+
+# 设置图标文件的路径
+icon = os.path.join(os.getcwd(), "icon.ico")
+
+# 创建快捷方式的命令
+shortcut_cmd = f'{pythonw_path} {main_path}'
+
+# 创建桌面快捷方式
+desktop_shortcut_path = str(desktop / "MySwitcherProgram.lnk")
+with winshell.shortcut(desktop_shortcut_path) as link:
+    link.path = pythonw_path
+    link.arguments = main_path
+    link.icon_location = (icon, 0)
+    link.description = "My Switcher Program"
+
+# 获取开始菜单的路径
+start_menu = Path(wpath.get_path(FOLDERID.StartMenu))
+
+# 创建开始菜单快捷方式
+start_menu_shortcut_path = str(start_menu / "MySwitcherProgram.lnk")
+with winshell.shortcut(start_menu_shortcut_path) as link:
+    link.path = pythonw_path
+    link.arguments = main_path
+    link.icon_location = (icon, 0)
+    link.description = "My Switcher Program"
